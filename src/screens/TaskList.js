e309a96c3d35ae60cb3f0e3553b0365fa5543511
Pedro Augusto <pedroet19@gmail.com>
2023-10-10 15:11:38 -0300

    Em andamento

---
+++ src/screens/TaskList.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react'
+import {View, Text, ImageBackground, StyleSheet} from 'react-native'
+
+import commomStyles from '../commomStyles'
+import todayImage from '../../assets/imgs/today.jpg'
+
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+import Task from '../components/Task'
+
+
+export default class TaskList extends Component {
+    render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMM')
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage}
+                style={styles.background}>
+                <View style={styles.titleBar}>
+                    <Text style={styles.title}>Hoje</Text>
+                    <Text style={styles.subTitle}>{today}</Text>
+                </View>
+                </ImageBackground>
+                <View style={styles.TaskList}>
+                    <Task desc="Comprar Livro" estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task desc="Ler Livro" estimateAt={new Date()}
+                    doneAt={null} />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3,
+    },
+    TaskList: {
+        flex: 7,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    
+    },
+    title: {
+        fontFamily: commomStyles.fontFamily,
+        color: commomStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20,
+
+    },
+    subTitle: {
+        fontFamily: commomStyles.fontFamily,
+        color: commomStyles.colors.secondary,
+        fontSize: 30,
+        marginLeft: 20,
+        marginBottom: 30,
+    }
+
+})
